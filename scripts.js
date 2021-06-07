fd4a456a30d01b56f779fc7f2f19c1991b29af62
Francisco Morales Lopez <famlo95@gmail.com>
2021-06-06 21:03:39 -0600

    added some comments and renamed some variables

--- scripts.js
+++ scripts.js
@@ -1,47 +1,47 @@
 const screen = document.querySelector('.solution');
-const eraser = document.querySelector('#erase-btn');
-const cleaner = document.querySelector('#clear-btn');
-const numbers = document.querySelector('.clear-numbers-container');
+const numbersPanel = document.querySelector('.clear-numbers-container');
 const operators = document.querySelector('.operators-container');
 const equalTo = document.querySelector('#equalTo');
+const eraser = document.querySelector('#erase-btn');
+const cleaner = document.querySelector('#clear-btn');
 let digit;
-let num;
+let number;
 let operator = '';
-let firstNum = 0;
-let nums = [];
+let firstNumber = 0;
+let numbers = [];
 let total = 0;
+let numberAdded;
 
 function showNumber() {
-    num = parseInt(nums.join(''));
-    screen.innerText = num;
+    number = parseInt(numbers.join(''));
+    screen.innerText = number;
 }
 
 function clearMemory() {
-    firstNum = num = 0;
-    screen.innerText = num;
-    console.log(`empty ${num}`);
+    firstNumber = number = 0;
+    screen.innerText = number;
 }
 
 cleaner.addEventListener('click', function () {
-    nums.splice(0);
+    numbers.splice(0);
     clearMemory();
 });
 
 eraser.addEventListener('click', function () {
     block_1: {
-        nums.pop();
+        numbers.pop();
         showNumber();
-        if (nums.length === 0) {
+        if (numbers.length === 0) {
             clearMemory();
             break block_1;
         }
     }
 });
 
-numbers.addEventListener('click', function (event) {
+numbersPanel.addEventListener('click', function (event) {
     if(event.target.className === 'calc-btn height number') {
         digit = event.target.innerText;
-        nums.push(digit);
+        numbers.push(digit);
         showNumber();
     }
 });
@@ -50,35 +50,35 @@ operators.addEventListener('click', function (event) {
     if (event.target.className === 'op-btn height') {
         operator = event.target.innerText;
         eraser.disabled = true;
-        firstNum = num;
-        nums.splice(0);
+        firstNumber = number;
+        numbers.splice(0);
     }
 });
 
 equalTo.addEventListener('click', function () {
     if(operator === '+'){
-        total = firstNum + num;
-        num = total;
-        nums = Array.from(String(num), Number);
+        total = firstNumber + number;
+        number = total;
+        numbers = Array.from(String(number), Number);
         showNumber();
     } 
     else if (operator === 'x') {
-        total = firstNum * num;
-        num = total;
-        nums = Array.from(String(num), Number);
+        total = firstNumber * number;
+        number = total;
+        numbers = Array.from(String(number), Number);
         showNumber();
     }
     else if (operator === '/') {
-        total = firstNum / num;
-        num = total;
-        nums = Array.from(String(num), Number);
+        total = firstNumber / number;
+        number = total;
+        numbers = Array.from(String(number), Number);
         showNumber();
     }
     else {
-        total = firstNum - num;
-        num = total;
-        nums = Array.from(String(num), Number);
+        total = firstNumber - number;
+        number = total;
+        numbers = Array.from(String(number), Number);
         showNumber();
     }
-    eraser.disabled = false;
+    eraser.enabled = true;
 });
